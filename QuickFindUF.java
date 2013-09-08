7ceae89d752e60d80f6bdbf72737e9e2d2077a65
Robert Correiro <robertcorreiro@gmail.com>
2013-09-08 00:05:01 -0400

    Add QuickFindUF.java

---
+++ QuickFindUF.java
@@ -0,0 +1,24 @@
+public class QuickFindUF {
+	private int[] id;
+
+	public QuickFindUF(int N) {
+		id = new int[N];
+		for (int i = 0; i < N; i++)
+				id[i] = i;
+	}
+
+    public boolean connected(int p, int q) {
+        // Connected if they have the same id
+        return id[p] == id[q];
+    }
+
+	public void union(int p, int q) {
+        // Change all entries whose id is id[p] to id[q]
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < N; i++)
+            if (id[i] == pid)
+                id[i] = qid;
+	}
+
+}
