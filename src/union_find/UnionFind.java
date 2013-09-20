4f8cfa1f184268114bb4610220a45b559259f800
Robert Correiro <robertcorreiro@gmail.com>
2013-09-20 12:40:35 -0400

    add Javadoc to UnionFind.java

--- src/union_find/UnionFind.java
+++ src/union_find/UnionFind.java
@@ -1,4 +1,25 @@
+/** UnionFind.java
+ *  A simple interface for representing connected components
+ *  in an undirected graph using integers as node IDs.
+ */
 public interface UnionFind {
-    boolean isConnected(int i, int j);
-    void union(int i, int j);
+
+  /**
+   * Returns a boolean representing if one node is 
+   * connected to another.
+   * 
+   * @param i Index of first node
+   * @param j Index of second node
+   * @return  True if the nodes are connected, false otherwise
+   */
+  boolean isConnected(int i, int j);
+
+  /**
+   * Creates a connection between two nodes, merging their 
+   * components together.
+   * 
+   * @param i Index of first node
+   * @param j Index of second node
+   */
+  void union(int i, int j);
 }
